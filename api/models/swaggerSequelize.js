1104fa9f6dff7901e489e47aba184f908227d283
Georg Braun, HM <georg.braun@hm.edu>
2018-01-25 02:08:00 +0100

    Clean up swaggerSequelize.js

--- api/models/swaggerSequelize.js
+++ api/models/swaggerSequelize.js
@@ -1,13 +1,9 @@
 'use strict';
 
-console.log("====================================================");
-console.log("==== File "+__filename+"==================");
-console.log("====================================================");
-
 const Sequelize = require('sequelize');
 const swaggerSequelize = require('swagger-sequelize');
 const fs        = require('fs');
-const jsyaml      = require('js-yaml');
+const jsyaml    = require('js-yaml');
 const path      = require("path");
 
 // For Sequelize with SQLite:
@@ -22,11 +18,10 @@ const sequelize = new Sequelize(
                        /*username*/ null, 
                        /*password*/ null, 
                        /*options */ sequelizeOptions);
+swaggerSequelize.setDialect(sequelize.options.dialect);
 
 // Read Swagger-API-Spec as YAML and convert it to an object:
 const swaggerSpec = jsyaml.safeLoad(fs.readFileSync(path.join(__dirname, '../swagger/swagger.yaml'), 'utf8'));
-
-swaggerSequelize.setDialect(sequelize.options.dialect);
 
 module.exports = {
   sequelize,
